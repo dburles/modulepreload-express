9645e6d470ac785b21d15a49439354aa6b4bf666
David Burles <dburles@gmail.com>
2024-09-18 09:41:12 +1000

    update to modulepreload-link-relations 3

--- createModulePreloadMiddleware.mjs
+++ createModulePreloadMiddleware.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 
 import path from "node:path";
-import resolveLinkRelations from "modulepreload-link-relations/resolveLinkRelations.mjs";
+import createResolveLinkRelations from "modulepreload-link-relations/createResolveLinkRelations.mjs";
 import formatLinkHeaderRelations from "modulepreload-link-relations/formatLinkHeaderRelations.mjs";
 
 /** @typedef {import('express').RequestHandler} RequestHandler **/
+/** @typedef {import('modulepreload-link-relations/createResolveLinkRelations.mjs').AsyncMapLike} AsyncMapLike */
 
 const defaultOptions = {
   extensions: ["mjs", "js"],
@@ -12,9 +13,11 @@ const defaultOptions = {
 
 /**
  * Creates a middleware function that adds a Link header containing modulepreload link relationships to JavaScript module requests.
- * @param {string} appPath The relative path to the application root directory, eg "app".
+ * @param {string} appPath Path to the application root directory, eg "app".
  * @param {Object} [userOptions] The options object.
  * @param {Array<string>} [userOptions.extensions] The file extensions to consider for module scripts.
+ * @param {string} [userOptions.importMap] Import map.
+ * @param {AsyncMapLike} [userOptions.cache] A custom cache.
  * @returns {RequestHandler} The middleware function.
  */
 export default function createModulePreloadMiddleware(
@@ -26,19 +29,28 @@ export default function createModulePreloadMiddleware(
     ...userOptions,
   };
 
+  const resolveLinkRelations = createResolveLinkRelations(appPath, {
+    importMap: options.importMap,
+    cache: options.cache,
+  });
+
   return async function modulePreloadMiddleware(req, res, next) {
-    const url = req.url;
-    const file = req.url?.slice(1);
-    const fileExt = file && path.extname(file).slice(1);
+    try {
+      const url = req.url;
+      const file = req.url?.slice(1);
+      const fileExt = file && path.extname(file).slice(1);
 
-    if (fileExt && options.extensions.includes(fileExt)) {
-      const linkRelations = await resolveLinkRelations({ appPath, url });
+      if (fileExt && options.extensions.includes(fileExt)) {
+        const linkRelations = await resolveLinkRelations(url);
 
-      if (linkRelations) {
-        res.append("Link", formatLinkHeaderRelations(linkRelations));
+        if (linkRelations) {
+          res.append("Link", formatLinkHeaderRelations(linkRelations));
+        }
       }
-    }
 
-    next();
+      next();
+    } catch (error) {
+      next(error);
+    }
   };
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # modulepreload-express
 
-Express middleware for generating [modulepreload](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel/modulepreload) link relations for a [Link](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Link) entity-header based on the requested JavaScript modules import graph. This will prevent request waterfalls for nested module imports.
+Express middleware for generating [modulepreload](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel/modulepreload) link relations for a [Link](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Link) entity-header based on the requested JavaScript modules import graph. This will prevent request waterfalls for nested module imports. Supports import maps.
 
 ## Install
 
@@ -8,7 +8,7 @@ Express middleware for generating [modulepreload](https://developer.mozilla.org/
 npm i modulepreload-express
 ```
 
-## Usage Example
+## Usage example
 
 ```js
 import express from "express";
@@ -23,3 +23,13 @@ app.use(express.static(APP_ROOT));
 
 app.listen(3000);
 ```
+
+## API
+
+### `createModulePreloadMiddleware(path[, options])`
+
+- `path` {string} Path to the application root directory, eg "app".
+- `options` {Object}
+  - `extensions` {Array&lt;string&gt;} The file extensions to consider for module scripts. Defaults to: `["mjs", "js"]`.
+  - `importMap` {string} Import map string.
+  - `cache` {Object} A custom (map-like) cache.
